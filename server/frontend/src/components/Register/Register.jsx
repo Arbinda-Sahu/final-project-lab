39752ca5673a4cb2bb1199122c225f497404852e
Arbinda-Sahu <arbindakumarsahu13@gmail.com>
2026-04-29 23:38:16 +0530

    Implement registration form component

---
+++ server/frontend/src/components/Register/Register.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+
+function Register() {
+  const [form, setForm] = useState({
+    username: "",
+    first_name: "",
+    last_name: "",
+    email: "",
+    password: ""
+  });
+
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(form);
+  };
+
+  return (
+    <div>
+      <h2>Sign Up</h2>
+      <form onSubmit={handleSubmit}>
+        <input name="username" placeholder="Username" onChange={handleChange} /><br />
+        <input name="first_name" placeholder="First Name" onChange={handleChange} /><br />
+        <input name="last_name" placeholder="Last Name" onChange={handleChange} /><br />
+        <input name="email" type="email" placeholder="Email" onChange={handleChange} /><br />
+        <input name="password" type="password" placeholder="Password" onChange={handleChange} /><br />
+        
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+}
+
+export default Register;
